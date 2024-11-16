abdab80407cf0437c24dd1e7f138252912450d25
典宇 余 <yudianyu@outlook.com>
2024-11-17 00:15:56 +0800

    class shader

--- include/shader/shader.hpp
+++ include/shader/shader.hpp
@@ -5,15 +5,123 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <glad/glad.h>
 
 class Shader {
 public:
 	unsigned int ID;
 	Shader(const char* vertexPath, const char* fragmentPaht);
+	void compile();
 	void use();
 	void setBool(const std::string &name, bool value);
 	void setInt(const std::string &name, int value);
-	void setFloat(const std::string &name, float value);
+	void setFloat(const std::string& name, float value);
+	const char* getVertexShderCode() const;
+	const char* getFragmentShderCode() const;
+private:
+	const char *vShaderCode, *fShaderCode;
+	static std::string readShaderCodeFromFile(const char* path);
 };
+
+Shader::Shader(const char* vertexPath, const char* fragmentPaht): vShaderCode(readShaderCodeFromFile(vertexPath).c_str()), fShaderCode(readShaderCodeFromFile(fragmentPath).c_str()) {
+	
+}
+
+inline void Shader::compile()
+{
+	int  success;
+	char infoLog[512];
+
+	unsigned int vertex, fragment;
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, vShaderCode, NULL);
+	glCompileShader(vertex);
+
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, fShaderCode, NULL);
+	glCompileShader(fragment);
+
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader_2);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	ID = shaderProgram;
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+inline void Shader::use()
+{
+	glUseProgram(ID);
+}
+
+inline void Shader::setBool(const std::string& name, bool value)
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+inline void Shader::setInt(const std::string& name, int value)
+{
+	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+inline void Shader::setFloat(const std::string& name, float value)
+{
+	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+inline const char* Shader::getVertexShderCode() const{
+	return vShaderCode;
+}
+
+inline const char* Shader::getFragmentShderCode() const {
+	return fShaderCode;
+}
+
+inline std::string Shader::readShaderCodeFromFile(const char* filePath)
+{
+	std::string shaderCode;
+	std::ifstream shaderFile;
+
+	// 保证ifstream对象可以抛出异常：
+	shaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+	try {
+		shaderFile.open(filePath);
+
+		std::stringstream shaderStream;
+		shaderStream << shaderFile.rdbuf();
+
+		shaderFile.close();
+
+		shaderCode = shaderStream.str();
+	}
+	catch (std::ifstream::failure e) {
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	return shaderCode;
+}
 
 #endif
--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,50 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
+#include <shader/shader.hpp>
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+void processInput(GLFWwindow* window);
+
 int main() {
 	glfwInit();
-	// 主要版本与最低版本提示
+	// 主要版本与最低版本提示、OPENGL版本提示
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "my opengl proj", NULL, NULL);
+	if (window != NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	Shader out_shader("resource/shader/first_vertex_shader.vs", "resource/shader/first_fragment_shader.fs");
+
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
